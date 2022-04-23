83d2ea5a7ed308f895e643e13cf31687d9a865cc
roger-lcc <953120129@qq.com>
2022-04-23 14:11:02 +0800

    test

--- benchmarks/benchmarks/soiling.py
+++ benchmarks/benchmarks/soiling.py
@@ -24,3 +24,7 @@ class Soiling:
     def time_kimber(self, ndays):
         cleaningthreshold = 25
         soiling.kimber(self.rainfall, cleaningthreshold)
+
+    def time_test(self, ndays):
+        cleaningthreshold = 0.5
+        soiling.hsu(self.rainfall, )
